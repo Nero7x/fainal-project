b035ffe6fe07fe02d00cf7d29e269211c960f4c1
ali <alitalal096@gmail.com>
2024-05-10 20:13:16 +0300

    add select in form and connect with backend

--- project/app.py
+++ project/app.py
@@ -11,15 +11,18 @@ def home():
 def upload_file():
     message = ''
     if request.method == 'POST':
+        diagram_type = request.form.get('diagrams')
         file = request.files['file']
         filename = file.filename
         file_extension = os.path.splitext(filename)[1]
         
         if file_extension in ['.txt', '.pdf']:
-            DAnalysis.dataInitialization(file,file_extension)
+            DAnalysis.dataInitialization(file,file_extension,diagram_type)
             return redirect(url_for('diagram'))
         else:
             message = 'Invalid file type. Please upload a .txt or .pdf file.'
+
+        diagram_type = request.form.get('diagrams')
     return render_template('input.html', message=message)
 
 
--- project/server/DocumentAnalysis.py
+++ project/server/DocumentAnalysis.py
@@ -1,35 +1,13 @@
 import io
 from PyPDF2 import PdfReader 
-import os
-import tempfile
 import spacy
-import fitz
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import nltk as nltk
-import string
 from server import ElemenrsFinder as EF
 from server import ReletionshipFinder as RF
-#doc = "./client/nfr.csv"
-#req_data = pd.read_csv(doc, delimiter=':', nrows=5, header=None)
-#req_data.columns= ['type', 'req']
 
-#pd.set_option('display.max_colwidth', 100)
-#print(req_data.head(5))
 
-#def remove_punct(text):
-    #text_nopuct = "".join([char for char in text if char not in string.punctuation])
-    #return text_nopuct
-    
-
-#df=req_data.copy()
-
-#df['req_nopunct'] = df['req'].apply(lambda x: remove_punct(x.lower()))
-#print(df.head(5))
-
-def dataInitialization(file,file_extension):
+def dataInitialization(file,file_extension,diagram_type):
+    print(diagram_type)
     texts = ""
     if file_extension == ".txt":
         text = file.read().decode('utf-8') 
@@ -67,9 +45,5 @@ def dataInitialization(file,file_extension):
     for i,result in enumerate(results):
         print(f"{i}- {result}: {results[result]}")
 
-
-if __name__ == '__main__':
-    document = "req.txt"
-    dataInitialization(document)
 
 
